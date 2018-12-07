5edd8d07f71bb4ac7a0f2935baeb4284d19116bb
rickey liao <micliao@gmail.com>
2018-12-07 02:50:16 +0000

    add to repo

---
+++ constant.go
@@ -0,0 +1,6 @@
+package nbsnetwork
+
+const (
+	IP_TYPE_IP4 = 0
+	IP_TYPE_IP6 = 1
+)
--- udpaddress.go
+++ udpaddress.go
@@ -1,12 +1,48 @@
 package nbsnetwork
 
-
+import (
+	"github.com/kprc/nbsdht/nbserr"
+	"strings"
+)
 
 type UdpAddresser interface {
 
 }
 
 
+type address struct {
+	ip6type bool
+	addr []byte		//big-endian
+	port uint16
+}
+
 type udpAddress struct {
+	addrs []address
+}
+
+func NewUdpAddress() UdpAddresser  {
+	var addrs udpAddress
+
+	addrs.addrs = make([]address,0)
+
+	return &addrs
+}
+
+func (uaddr *udpAddress)Append(addr address) {
+	uaddr.addrs = append(uaddr.addrs, addr)
+}
+
+func (uaddr *udpAddress)AddIP4(ipstr string, port uint16) error {
+	sarr := strings.Split(ipstr,".")
+
+	if len(sarr) != 4 {
+		return &nbserr.NbsErr{ErrId:nbserr.UDP_ADDR_PARSE,Errmsg:"Parse ip4 address fault"}
+	}
+
+
+
+}
+
+func (uaddr *udpAddress)Add6(ipstr string, port uint16)  {
 
 }
