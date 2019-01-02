d8df4ecfb1384710f9fb096898cc331578ab61d7
rickey liao <micliao@gmail.com>
2019-01-02 13:06:49 +0000

    add to repo

--- client/udpsafeclient.go
+++ client/udpsafeclient.go
@@ -16,6 +16,20 @@ type UdpClient interface {
 	Send(r io.ReadSeeker) error
 }
 
+func NewUdpClient(ip string,port uint16) UdpClient {
+	uc := &udpClient{dialAddr:address.NewUdpAddress(),processWait:make(chan int,1024)}
+	uc.dialAddr.AddIP4(ip,port)
+
+	return uc
+}
+
+func (uc *udpClient)Dial()  {
+	
+}
+
+func (uc *udpClient)Send(r io.ReadSeeker) error  {
 
 
 
+	return nil
+}
