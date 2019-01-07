7a4cfed887d2ec76ef2c6beda0f0b3ab683fc43b
rickey liao <micliao@gmail.com>
2019-01-07 06:33:31 +0000

    add to repo

--- test/client/testclient.go
+++ test/client/testclient.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main()  {
-	c := client.NewUdpClient("192.168.103.66","",11223,0)
+	c := client.NewUdpClient("192.168.107.242","",11223,0)
 	c.Dial()
 
 	c.SendBytes([]byte("Title Ping")[:],constant.MSG_PING,"10366servber",[]byte("PING TO SERVER")[:])
