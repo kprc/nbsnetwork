70e137667bd6aa15b73895b0073dea01fb88c2a9
rickey liao <micliao@gmail.com>
2019-02-25 07:16:19 +0000

    add to repo

--- netcommon/udpconn.go
+++ netcommon/udpconn.go
@@ -7,6 +7,16 @@ import (
 	"sync"
 	)
 
+var (
+	CONNECTION_INIT int32 = 0
+	STOP_CONNECTION int32 = 1
+	BAD_CONNECTION int32 = 2
+	CONNECTION_RUNNING int32 = 3
+
+
+)
+
+
 type udpconn struct {
 	addr *net.UDPAddr
 	sock *net.UDPConn
@@ -15,10 +25,8 @@ type udpconn struct {
 	recvFromConn chan interface{}
 	tick chan int64
 
-	status int32 //0 not set, 1 stopped, 2 bad connection,
-
-	runninglock sync.Mutex
-	isrunning int32 //0 not running, 1 running
+	statuslock sync.Mutex
+	status int32 //0 not set, 1 stopped, 2 bad connection, 3 connecting
 
 	stopsign chan int
 
@@ -30,14 +38,17 @@ type udpconn struct {
 
 type UdpConn interface {
 	Connect() error
-	Send(v interface{}) error
+	Send(data [] byte) error
+	Read() ([]byte, error)
 	SetTimeout(tv int)
+	Status() bool
 }
 
- var (
- 	baderr = nbserr.NbsErr{ErrId:nbserr.UDP_BAD_CONN,Errmsg:"Bad Connection"}
-
- )
+var (
+	baderr = nbserr.NbsErr{ErrId:nbserr.UDP_BAD_CONN,Errmsg:"Bad Connection"}
+	notreadyerr = nbserr.NbsErr{ErrId:nbserr.UDP_CONN_NOTREADY,Errmsg:"Connection not ready!"}
+	nodataerr = nbserr.NbsErr{ErrId:nbserr.UDP_CONN_NODATA,Errmsg:"Connection have no data arrived"}
+)
 
 func NewUdpConn(addr *net.UDPAddr,sock *net.UDPConn,isconn bool) UdpConn {
 	uc:= &udpconn{}
@@ -51,9 +62,8 @@ func NewUdpConn(addr *net.UDPAddr,sock *net.UDPConn,isconn bool) UdpConn {
 	uc.sock = sock
 	uc.isconn = isconn
 
-	nt:=tools.GetNbsTickerInstance()
+	nt := tools.GetNbsTickerInstance()
 	nt.Reg(&uc.tick)
-
 	uc.timeouttv = 1000   //ms
 
 	return uc
@@ -69,7 +79,16 @@ func (uc *udpconn)Connect() error{
 		return baderr
 	}
 
-	//start send and recv
+	if uc.isconn == false {
+		wg := &sync.WaitGroup{}
+		//start send and recv
+		wg.Add(1)
+
+		go uc.recv(wg)
+
+		defer wg.Wait()
+	}
+
 	for{
 		select {
 			case data2send:=<-uc.ready2send:
@@ -87,8 +106,30 @@ func (uc *udpconn)Connect() error{
 
 		}
 	}
+}
+
+
+func (uc *udpconn)recv(wg *sync.WaitGroup) error{
+	defer wg.Done()
+
+	n:=1024
+	roundbuf := make([]byte,n)
+
+	for {
+		buf := roundbuf[0:n]
+
+		var err error
+		var nr int
+		if  nr,err = uc.sock.Read(buf); err!=nil{
+			return err
+		}
+		uc.recvFromConn <- buf[0:nr]
+	}
+
+	return nil
 
 }
+
 
 func (uc *udpconn)sendkapacket() error {
 
@@ -103,10 +144,36 @@ func (uc *udpconn)sendpacket(v interface{})  error{
 }
 
 
-func (uc *udpconn)Send(v interface{}) error  {
+func (uc *udpconn)Send(data []byte) error  {
 
 	return nil
 }
 
+func (uc *udpconn)Status() bool  {
 
+}
+
+func (uc *udpconn)Read() ([]byte,error)  {
+
+	if uc.status != CONNECTION_RUNNING {
+		return nil,notreadyerr
+	}
+
+	ret := <-uc.recvFromConn
+
+	return ret.([]byte),nil
+}
+
+func (uc *udpconn)ReadyAnSyc() ([]byte,error)  {
+	if uc.status != CONNECTION_RUNNING {
+		return nil,notreadyerr
+	}
+
+	select {
+	case ret:=<-uc.recvFromConn:
+		return ret.([]byte),nil
+	default:
+		return nil,nodataerr
+	}
+}
 
