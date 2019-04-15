fc523679938b3badbf259def4f723bee05833806
rickey liao <micliao@gmail.com>
2019-04-15 16:04:39 +0800

    add to repo

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package nbsnetwork
 import (
 	"github.com/kprc/nbsnetwork/tools"
 	"github.com/kprc/nbsnetwork/file"
+	"github.com/kprc/nbsnetwork/translayer/store"
 )
 
 func init()  {
@@ -10,4 +11,21 @@ func init()  {
 	go tick.Run()
 
 	file.FileRegister()
+
+
+	runstore()
+}
+
+
+func runstore(){
+	fs:=file.GetFileStoreInstance()
+	go fs.Run()
+
+	msgstore:=store.GetBlockStoreInstance()
+	go msgstore.Run()
+
+	ss:=store.GetStreamStoreInstance()
+
+	go ss.Run()
+
 }
